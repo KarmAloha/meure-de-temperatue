54450c30377f2e74f76e6b7d26ec9e7f25a29f70
KarmAloha <103488356+KarmAloha@users.noreply.github.com>
2022-04-12 14:37:48 +0200

    Fichiers initiaux pour un projet MakeCode

---
+++ main.ts
@@ -0,0 +1,19 @@
+basic.forever(function () {
+    if (input.buttonIsPressed(Button.A)) {
+        basic.clearScreen()
+        basic.showString("T=")
+        basic.showNumber(input.temperature())
+        basic.showString("Degres")
+    } else {
+        basic.showLeds(`
+            . . # . .
+            . # . . .
+            # # # # #
+            . # . . .
+            . . # . .
+            `)
+    }
+})
+basic.forever(function () {
+	
+})
